f1b46605463af2c7cf6f9b481e23ef7e688dc7b3
guadalupe atim <guadiiatim1309@gmail.com>
2023-12-08 20:31:41 -0300

    saque id

--- ViewModels/CrearUsuarioViewModel.cs
+++ ViewModels/CrearUsuarioViewModel.cs
@@ -6,16 +6,8 @@ namespace Kanban.ViewModels;
 
 public class CrearUsuarioViewModel
 {
-    [Required(ErrorMessage = "Complete el campo")]
-    private int id;
-
-    [Required(ErrorMessage = "Complete el campo")]
     private string nombreDeUsuario;
-
-    [Required(ErrorMessage = "Complete el campo")]
     private string contrasenia;
-
-    [Required(ErrorMessage = "Complete el campo")]
     private Rol rol;
 
     public CrearUsuarioViewModel()
@@ -24,13 +16,11 @@ public class CrearUsuarioViewModel
 
     public CrearUsuarioViewModel(Usuario usuarioNuevo)
     {
-        this.id = usuarioNuevo.Id;
         this.nombreDeUsuario = usuarioNuevo.NombreDeUsuario;
         this.contrasenia = usuarioNuevo.Contrasenia;
         this.rol = usuarioNuevo.Rol;
     }
 
-    public int Id { get => id; set => id = value; }
     public string NombreDeUsuario { get => nombreDeUsuario; set => nombreDeUsuario = value; }
     public string Contrasenia { get => contrasenia; set => contrasenia = value; }
     public Rol Rol { get => rol; set => rol = value; }
