56b0b68b29275ef910a014b88ac5d571c28529c8
guadalupe atim <guadiiatim1309@gmail.com>
2023-12-11 15:17:58 -0300

    permitir nulos

--- ViewModels/TareaViewModel.cs
+++ ViewModels/TareaViewModel.cs
@@ -12,7 +12,7 @@ public class TareaViewModel
     private EstadoTarea estado;
     private string? descripcion;
     private string? color;
-    private int idUsuarioAsignado;
+    private int? idUsuarioAsignado;
 
     public TareaViewModel()
     {
@@ -32,7 +32,7 @@ public class TareaViewModel
     public int IdTablero { get => idTablero; set => idTablero = value; }
     public string Nombre { get => nombre; set => nombre = value; }
     public EstadoTarea Estado { get => estado; set => estado = value; }
-    public string Descripcion { get => descripcion; set => descripcion = value; }
-    public string Color { get => color; set => color = value; }
-    public int IdUsuarioAsignado { get => idUsuarioAsignado; set => idUsuarioAsignado = value; }
+    public string? Descripcion { get => descripcion; set => descripcion = value; }
+    public string? Color { get => color; set => color = value; }
+    public int? IdUsuarioAsignado { get => idUsuarioAsignado; set => idUsuarioAsignado = value; }
 }
