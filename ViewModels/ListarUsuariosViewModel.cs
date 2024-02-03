04fff1c2dca87f78ba6f6eae35b77a1a8ab4c37f
guadalupe atim <guadiiatim1309@gmail.com>
2024-02-03 19:49:11 -0300

    agrego atributo

--- ViewModels/ListarUsuariosViewModel.cs
+++ ViewModels/ListarUsuariosViewModel.cs
@@ -7,7 +7,20 @@ namespace Kanban.ViewModels;
 public class ListarUsuariosViewModel
 {
     private List<UsuarioViewModel> usuariosVM;
+    private UsuarioViewModel usuarioLoggeado;
     
+    public ListarUsuariosViewModel(List<Usuario> usuarios, UsuarioViewModel usuarioLoggeado)
+    {
+        this.UsuarioLoggeado = usuarioLoggeado;
+        usuariosVM = new List<UsuarioViewModel>();
+
+        foreach (var usuario in usuarios)
+        {
+            UsuarioViewModel usuarioNuevo = new UsuarioViewModel(usuario);
+            usuariosVM.Add(usuarioNuevo);
+        }
+    }
+
     public ListarUsuariosViewModel(List<Usuario> usuarios)
     {
         usuariosVM = new List<UsuarioViewModel>();
@@ -18,6 +31,6 @@ public class ListarUsuariosViewModel
             usuariosVM.Add(usuarioNuevo);
         }
     }
-
     public List<UsuarioViewModel> UsuariosVM { get => usuariosVM; set => usuariosVM = value; }
+    public UsuarioViewModel UsuarioLoggeado { get => usuarioLoggeado; set => usuarioLoggeado = value; }
 }
