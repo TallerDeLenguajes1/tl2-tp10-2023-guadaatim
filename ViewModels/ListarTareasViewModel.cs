9862279543ac3d511e66dbc2c8382f2426188ae1
guadalupe atim <guadiiatim1309@gmail.com>
2024-02-03 19:51:36 -0300

    tareas propias y recibe vm

--- ViewModels/ListarTareasViewModel.cs
+++ ViewModels/ListarTareasViewModel.cs
@@ -6,21 +6,29 @@ namespace Kanban.ViewModels;
 
 public class ListarTareasViewModel
 {
-    List<TareaViewModel> tareasVM;
+    private List<TareaViewModel> tareasVM;
+    private List<TareaViewModel> tareasPropiasVM;
 
     public ListarTareasViewModel()
     {
     }
 
-    public ListarTareasViewModel(List<Tarea> tareas)
+    public ListarTareasViewModel(List<TareaViewModel> tareas, List<TareaViewModel> tareasPropias)
     {
         tareasVM = new List<TareaViewModel>();
-        
-        foreach (var t in tareas)
+        tareasPropiasVM = new List<TareaViewModel>();
+
+        foreach (var tarea in tareasPropias)
         {
-            TareaViewModel tarea = new TareaViewModel(t);
+            tareasPropiasVM.Add(tarea);
+        }
+
+        foreach (var tarea in tareas)
+        {
             tareasVM.Add(tarea);
         }
     }
+
     public List<TareaViewModel> TareasVM { get => tareasVM; set => tareasVM = value; }
+    public List<TareaViewModel> TareasPropiasVM { get => tareasPropiasVM; set => tareasPropiasVM = value; }
 }
