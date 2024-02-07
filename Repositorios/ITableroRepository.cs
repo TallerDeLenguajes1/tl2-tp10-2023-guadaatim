796ba1d7e8f3ea7601f830dfab0590ad00017077
guadalupe atim <guadiiatim1309@gmail.com>
2024-02-07 17:48:52 -0300

    agrego metodos

--- Repositorios/ITableroRepository.cs
+++ Repositorios/ITableroRepository.cs
@@ -6,10 +6,12 @@ public interface ITableroRepository
 {
     public void CreateTablero(Tablero tablero);
     public void UpdateTablero(Tablero tableroModificar);
+    public Tablero GetTableroById(int idTablero);
+    public bool PerteneceTablero(int idUsuario, int idTablero);
+    public List<Tablero> GetTableroByUsuario(int idUsuario);
     public List<Tablero> GetAllTableros();
     public List<TableroViewModel> GetTablerosViewModel();
-    public List<TableroViewModel> GetTableroViewModelsByUsuario(int idUsuario);
-    public Tablero GetTableroById(int idTablero);
-    public List<Tablero> GetTableroByUsuario(int idUsuario);
+    public List<TableroViewModel> GetTablerosViewModelByUsuario(int idUsuario);
+    public List<TableroViewModel> GetTablerosViewModelByTarea(int idUsuario);
     public void DeleteTablero(int idTablero);
 }
