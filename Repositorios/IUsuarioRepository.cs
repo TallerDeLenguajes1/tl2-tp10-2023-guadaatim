e3b90aaeba62962c2f82af27b79b4400af099219
guadalupe atim <guadiiatim1309@gmail.com>
2024-02-07 17:44:08 -0300

    agrego dos metodos

--- Repositorios/IUsuarioRepository.cs
+++ Repositorios/IUsuarioRepository.cs
@@ -6,8 +6,10 @@ public interface IUsuarioRepository
 { 
     public void CreateUsuario(Usuario usuarioNuevo);
     public bool ExisteUsuario(string nombreUsuario);
+    public bool ExisteUsuarioLogin(Usuario usuario);
     public void UpdateUsuario(Usuario usuarioModificar);
     public List<Usuario> GetAllUsuarios();
+    public List<Usuario> GetAllUsuariosExcept(int idUsuario);
     public Usuario GetUsuarioByNombre(string nombreUsuario);
     public Usuario GetUsuarioById(int idUsuario);
     public void DeleteUsuario(int idUsuario);
