c400726fc0e732df6ece81681a942cc972899a38
guadalupe atim <guadiiatim1309@gmail.com>
2024-02-07 17:48:22 -0300

    agrego dos metodos y cambio lo q devuelve uno

--- Repositorios/ITareaRepository.cs
+++ Repositorios/ITareaRepository.cs
@@ -10,9 +10,9 @@ public interface ITareaRepository
     public List<Tarea> GetAllTareas();
     public Tarea GetTareaById(int idTarea);
     public List<Tarea> GetAllTareasByUsuario(int idUsuario);
-    public List<Tarea> GetAllTareasByTablero(int idTablero);
+    public List<TareaViewModel> GetAllTareasByTablero(int idTablero);
     public List<TareaViewModel> GetTareasViewModel();
-    public List<TareaViewModel> GetTareaViewModelByUsuario(int idTarea);
-    public void AsignarUsuario(int idUsuario, int idTarea);
+    public List<TareaViewModel> GetTareasViewModelByUsuario(int idUsuario);
+    public TareaViewModel GetTareaViewModel(int idUsuario, int idTablero);
     public void DeleteTarea(int idTarea);
 }
